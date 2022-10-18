d8a8e6c6c184c9ec6b64cc23eeebf90f94f5134b
김지은 <kimjieun@gimjieun-ui-MacBookAir.local>
2022-10-19 00:40:14 +0900

    axios 사용 예제

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+
+/*
 //const data = { authId: 'member' };
 
 fetch('http://localhost:8080/init/menu?authId=member', {
@@ -14,3 +16,16 @@ fetch('http://localhost:8080/init/menu?authId=member', {
 .catch((error) => {
   console.error('실패:', error);
 });
+*/
+
+  axios({
+    method: 'get', //통신 방식
+    url: '/api/init/test', //통신할 페이지
+    data: {} //인자로 보낼 데이터
+  })
+    .then(response=>{
+        console.log(response.data);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
